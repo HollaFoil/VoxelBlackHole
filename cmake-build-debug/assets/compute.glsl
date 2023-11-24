fce2d90fccb4534f279861600dfb6e2241faed28
HollaFoil <nedas60@gmail.com>
2023-11-24 12:15:03 +0100

    Made mathematical operations more efficient

--- cmake-build-debug/assets/compute.glsl
+++ cmake-build-debug/assets/compute.glsl
@@ -2,7 +2,7 @@
 precision highp float;
 
 
-layout(local_size_x = 32, local_size_y = 32) in;
+layout(local_size_x = 512, local_size_y = 1) in;
 layout(rgba32f, binding = 0) uniform image2D img_output;
 
 #define SMOOTH 0
@@ -14,52 +14,71 @@ const float width = 1536.0f;
 const float height = 864.0f;
 const vec2 scr_size = vec2(width, height);
 const vec3 centerOfSphere = vec3(0.5f);
-const float grid_size = 0.5f;
+const float grid_size = 0.10f;
 
-float sphere_size = sin(time/5.0f)*4.0f + 8.4f;
+float sphere_size = 300.0f - sin(time/3)*200;
+
+float distSquared( vec3 A, vec3 B )
+{
+    vec3 C = A - B;
+    return dot( C, C );
+
+}
 
 vec4 castRay(vec3 origin, vec3 direction) {
     const float sign_x = direction.x > 0 ? 1 : -1;
     const float sign_y = direction.y > 0 ? 1 : -1;
     const float sign_z = direction.z > 0 ? 1 : -1;
-    const float offset_x = direction.x > 0 ? 0 : 1;
-    const float offset_y = direction.y > 0 ? 0 : 1;
-    const float offset_z = direction.z > 0 ? 0 : 1;
-    const int max_steps = 200;
+    const float offset_x = direction.x > 0 ? 1 : 0;
+    const float offset_y = direction.y > 0 ? 1 : 0;
+    const float offset_z = direction.z > 0 ? 1 : 0;
+    const int max_steps = 650;
+
+
+    vec3 sign = vec3(sign_x, sign_y, sign_z);
+    vec3 offset = vec3(offset_x, offset_y, offset_z);
+    vec3 inv = 1/direction;
+
 
     vec3 pos = origin;
-    vec3 tile_coords = floor(pos/grid_size); // Maybe needs to be * instead, idk
+    vec3 tile_coords = floor(pos/grid_size);
+    vec3 tile_coords_scaled = tile_coords*grid_size;// Maybe needs to be * instead, idk
+    vec3 sign_scaled = sign*grid_size;
+    vec3 offset_scaled = offset*grid_size;
 
-    float dist = distance(tile_coords, centerOfSphere);
+
+
+    float dist = distSquared(tile_coords_scaled, centerOfSphere);
     int step = 0;
 
     int lastdir = -1;
-    while (dist > sphere_size && step < max_steps) {
+    while (step < max_steps && dist > sphere_size) {
         step++;
-        float dtx = ((tile_coords.x + sign_x + offset_x)*grid_size - pos.x) / direction.x;
-        float dty = ((tile_coords.y + sign_y + offset_y)*grid_size - pos.y) / direction.y;
-        float dtz = ((tile_coords.z + sign_z + offset_z)*grid_size - pos.z) / direction.z;
-        if (dtx < dty && dtx < dtz) {
-            pos += direction*dtx;
+        vec3 dt = (tile_coords_scaled+offset_scaled - pos)*inv;
+        if (dt.x < dt.y && dt.x < dt.z) {
+            pos += direction*dt.x;
             tile_coords.x += sign_x;
+            tile_coords_scaled.x += sign_scaled.x;
             lastdir = 0;
         }
-        else if (dty < dtz) {
-            pos += direction*dty;
+        else if (dt.y < dt.z) {
+            pos += direction*dt.y;
             tile_coords.y += sign_y;
+            tile_coords_scaled.y += sign_scaled.y;
             lastdir = 1;
         }
         else {
-            pos += direction*dtz;
+            pos += direction*dt.z;
             tile_coords.z += sign_z;
+            tile_coords_scaled.z += sign_scaled.z;
             lastdir = 2;
         }
 
-        dist = distance(tile_coords*grid_size, centerOfSphere);
+        dist = distSquared(tile_coords_scaled, centerOfSphere);
     }
     vec4 color;
     if (step == max_steps) color = texture(cubeMap, -normalize(direction));
-    else if (lastdir == 0) color = vec4(1,0,0,1) / (sphere_size + 5.0f - abs(pos.x));
+    else if (lastdir == 0) color = vec4(1,0,0,1) / (sqrt(sphere_size) + 2 - abs(tile_coords_scaled.x));
     else if (lastdir == 1) color = vec4(0,1,0,1) * 0;
     else color = vec4(0,0,1,1) * 0;
     return color;
